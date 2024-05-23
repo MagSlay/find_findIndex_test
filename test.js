da2e6f9925123fa596d7dec81058f19f7d2f23b6
Nicholas Kokotakis <njk51@georgetown.edu>
2024-05-23 15:28:06 -0400

    test

---
+++ test.js
@@ -0,0 +1,14 @@
+function findUserByUsername(userArray, username){
+    return userArray.find(function(user){
+        return user.username === username;
+    });
+};
+
+function removeUser(usersArray, username) {
+    let foundIndex = usersArray.findIndex(function(user){
+      return user.username === username;
+    });
+    if(foundIndex === -1) return;
+  
+    return usersArray.splice(foundIndex,1)[0];
+  };
